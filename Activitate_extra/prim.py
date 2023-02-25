e6197aeadf58d6d4531850469af6692ab89899f9
Kallo Alexandru Vlad <vlad807@yahoo.com>
2023-02-25 13:31:18 +0200

    Noi modificari

--- Activitate_extra/prim.py
+++ Activitate_extra/prim.py
@@ -1,5 +1,5 @@
 
 my_string = 'abcdefg'
-print(my_string[0:5:2])  #intre [] gasesc pozitia din string care o doresc, ex 0 este byte-ul din string, 5 pana la ce bit doresc
+print(my_string[0:5:2])  #intre [] gasesc pozitia din string pe care o doresc, ex 0 este byte-ul din string, 5 pana la ce bit doresc
 # sa merg, iar 2 este pasul din cati in cati biti vreau sa afisez.
 
