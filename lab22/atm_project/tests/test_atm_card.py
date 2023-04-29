58cc6f5558212adedc30a5e3d7d088e1b041cfba
Kallo Alexandru Vlad <vlad807@yahoo.com>
2023-04-29 10:47:28 +0300

    Test methods with unittest

--- lab22/atm_project/tests/test_atm_card.py
+++ lab22/atm_project/tests/test_atm_card.py
@@ -11,5 +11,22 @@ class TestConstructor(TestCase):
 
         self.assertIsInstance(card.number, str)
         self.assertEqual(len(card.number), 8)
-
         self.assertIsInstance(card.expire_date, datetime)
+        self.assertIsInstance(card.security_code, int)
+        self.assertGreaterEqual(card.security_code, 100)
+        self.assertLessEqual(card.security_code, 1000)
+
+
+class TestMethods(TestCase):
+
+    def test_get_balance(self):
+        test1 = Card('x', '1234')
+        test1.get_balance()
+        self.assertIsInstance(test1.get_balance, int)
+
+    def test_add_money(self):
+        test2 = Card('y', '2345')
+        test2.add_money(10)
+
+    def test_is_equal(self):
+        self.assertEqual(self.test_add_money, self.test_get_balance)
